317c1c44a78e86b9f64b54cdec745bf27fb272a0
sbeuhatsxb <mail2sbeuh@gmail.com>
2021-01-12 09:03:21 +0100

    Templating main form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends AbstractController
         // Get data from HTTP request
         $form->handleRequest($request);
         // Was the form submitted ?
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             // Deal with the submitted data
             // Get the Entity Manager
             $entityManager = $this->getDoctrine()->getManager();
--- src/Form/MessageSubmitType.php
+++ src/Form/MessageSubmitType.php
@@ -4,9 +4,9 @@ namespace App\Form;
 
 use App\Entity\Message;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,12 +15,29 @@ class MessageSubmitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
         $builder
             ->add('text', TextType::class)
-            ->add('type', TextType::class)
-            ->add('deathDate', DateType::class)
-            ->add('url', TextType::class)
-        ;
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Letter' => 'letter',
+                    'Note' => 'note',
+                    'Code' => 'code',
+                ],
+                'expanded' => true
+            ])
+            ->add('deathDate', ChoiceType::class, [
+                'choices' => [
+                    '6 hours' => '6',
+                    '12 hours' => '12',
+                    '24 hours' => '24',
+                    'a week' => 'week',
+                    'a month' => 'month',
+                    'a year' => 'year',
+                ],
+                'expanded' => true
+            ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
